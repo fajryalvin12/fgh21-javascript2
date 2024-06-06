5d90d073f9c5389399a77c3b8de1e6981251ed24
fajryalvin12 <165570283+fajryalvin12@users.noreply.github.com>
2024-06-06 20:52:53 +0700

    Add files via upload

--- Task2.js
+++ Task2.js
@@ -1,27 +1,91 @@
-const names = ["Abigail", "Alexandra", "Alison",
-"Amanda", "Angela", "Bella",
-"Carol", "Caroline", "Carolyn",
-"Deirdre", "Diana", "Elizabeth", 
-"Ella", "Faith","Olivia", "Penelope"]
+// const names = ["Abigail", "Alexandra", "Alison",
+// "Amanda", "Angela", "Bella",
+// "Carol", "Caroline", "Carolyn",
+// "Deirdre", "Diana", "Elizabeth", 
+// "Ella", "Faith","Olivia", "Penelope"]
 
-function searchName(search, arr, callback){
-    let result = [];
-    for (i = 0; i < arr.length; i++) {
-        if (arr[i].toLowerCase().includes(search)) {
-            result = [...result, arr[i]];
-        }
-    } 
-    callback(result, 3)
-}
+// function searchName(search, arr, callback){
+//     let result = [];
+//     for (i = 0; i < arr.length; i++) {
+//         if (arr[i].toLowerCase().includes(search)) {
+//             result = [...result, arr[i]];
+//         }
+//     } 
+//     callback(result, 3)
+// }
 
-function limit(arr, lim) {
-    let temp = []
-    for (i = 0; i < lim; i++) {
-        if(arr[i] !== undefined) {
-            temp = [...temp, arr[i]];
+// function limit(arr, lim) {
+//     let temp = []
+//     for (i = 0; i < lim; i++) {
+//         if(arr[i] !== undefined) {
+//             temp = [...temp, arr[i]];
+//         }
+//     }
+//     console.log(temp)
+// }
+
+const searchName = function (keyword, limit, cb) {
+    const name = ["Abigail", "Alexandra", "Alison",
+    "Amanda", "Angela", "Bella",
+    "Carol", "Caroline", "Carolyn",
+    "Deirdre", "Diana", "Elizabeth", 
+    "Ella", "Faith","Olivia", "Penelope"]; 
+
+    const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const lowercase = "abcdefghijklmnopqrstuvwxyz"
+    let searchResult = []
+
+        for (let i = 0; i < name.length; i++) {
+            const personName = name[i]
+            let result = ""
         }
+    
+    for (let i = 0; i < personName.length; i++) {
+        let converted = false 
+        for (let j = 0; j < uppercase.length; j++) {
+            if (converted === false)
+                if (personName[i] === uppercase[j]) {
+                result += lowercase[j]
+                converted = true
+                }
+        }
+        if (j < 25)
+            if (personName[i] !== uppercase[j]) {
+                result += personName[i]
+                converted = true
+            }
     }
-    console.log(temp)
+    
+    let isFound = false
+    for (let i = 0; i < result.length; i++){
+        let segment = ""
+        for (let j = i; j < keyword.length; j++)
+        if (result[j] !== undefined) {
+            segment += result[j]
+        }
+
+        if (isFound === true) 
+        searchResult = {...searchResult, personName}
+    }
+
+    cb (searchResult, limit)
+
+    function limit (segment, lim) {
+        let temp = []
+        for (let i = 0; i < lim; i++)
+            if(segment[i] !== undefined) {
+                temp = [...temp, segment[i]]
+            }       
+    }
+    console.log(temp)        
 }
 
-searchName("an", names, limit)
+// function limit(arr, lim) {
+//     let temp = []
+//     for (i = 0; i < lim; i++) {
+//         if(arr[i] !== undefined) {
+//             temp = [...temp, arr[i]];
+//         }
+//     }
+//     console.log(temp)
+// }
--- Task3.js
+++ Task3.js
@@ -1,14 +1,32 @@
-const dataArray = [1, 20, 45, 35, 12]
+const seleksiNilai = function(nilaiAwal, nilaiAkhir, dataArray){
 
-function seleksiNilai (nilaiAwal, nilaiAkhir, dataArray){
-    nilaiArr = dataArray.length
-    if(nilaiArr < 5){
-        console.log("Jumlah angka dalam dataArray harus lebih dari 5")
-    } else if (nilaiAkhir > nilaiAwal) {
-        console.log("Nilai akhir harus lebih besar daripada nilai awal")
-    } else if (nilaiArr < nilaiAwal) {
-        console.log("Nilai tidak ditemukan")
+if (nilaiAwal > nilaiAkhir){
+    console.log("Nilai akhir harus lebih besar dari nilai awal")
+}
+
+if (dataArray.length < 5) {
+    console.log("Jumlah angka dalam array harus lebih dari 5")
+}
+
+let result = []
+
+for(let i = 0; i < dataArray.length; i++){
+    const num = dataArray[i]
+    if(num > nilaiAwal){
+        if(num < nilaiAkhir)
+        result = [...result, num]
     }
 }
 
-seleksiNilai(15, 20, dataArray)
+    if (result.length === 0) {
+    console.log("Nilai tidak ditemukan")
+    } else {
+    console.log(result)
+    }   
+}
+
+seleksiNilai(5, 40, [2, 25, 4, 14, 17, 30, 8])
+
+const unsortedArray = function (seleksiNilai) {
+    
+}
